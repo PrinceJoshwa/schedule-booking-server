deeb8e2dcb008435733d288d5fdc2dbe94163b4b
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-03-06 16:44:15 +0530

    vercel commit

--- server.js
+++ server.js
@@ -12,13 +12,18 @@ connectDB();
 
 const app = express();
 
+// app.use(cors({
+//   origin: [
+//     process.env.CLIENT_URL,
+//     "https://schedura-landing.vercel.app"
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: [
-    process.env.CLIENT_URL,
-    "https://schedura-landing.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: ['http://localhost:5173', 'https://schedura-landing.vercel.app'],
   credentials: true
 }));
 
